535ef2ba96f939f5d97d841191f69d456e68c75f
Oshea <user@Osheas-MacBook-Pro.local>
2021-01-09 12:56:12 -0600

    Update app.js

--- comments/app.js
+++ comments/app.js
@@ -4,16 +4,28 @@ let ProfilePic = () => {
     </div>
 }
 
+let Gossip = () => {
+  return  <div>
+    <h3>Baby Shark doo odood doodododod</h3>
+    <h3>Baby Shark doo odood doodododod</h3>
+    <h3>Baby Shark doo odood doodododod</h3>
+    </div>
+}
+
 let CommentBody = () => {
     return <div class="userName">
 <h1> This Is A Bold UserName</h1>
-
+<h3>{Gossip()}</h3>
     </div>
 }
+
+
+
 let Comment = () => {
     return <div>
         <ProfilePic />
         <CommentBody />
+
     </div>
 }
 
